3925cd25067a8ac44e83ce79b43d7fad00b9773b
Atharv Phadnis <athiphadnis@gmail.com>
2022-10-13 23:51:13 +0530

    Updated Server URL to heroku

--- utils/client.js
+++ utils/client.js
@@ -1,6 +1,5 @@
-import Medusa from "@medusajs/medusa-js";
+import Medusa from "@medusajs/medusa-js"
 
-const BACKEND_URL =
-  process.env.NEXT_PUBLIC_MEDUSA_URL || "http://localhost:9000";
+const BACKEND_URL = process.env.NEXT_PUBLIC_MEDUSA_URL || "https://fakebazaar-medusajs.herokuapp.com"
 
-export const createClient = () => new Medusa({ baseUrl: BACKEND_URL });
+export const createClient = () => new Medusa({ baseUrl: BACKEND_URL })
